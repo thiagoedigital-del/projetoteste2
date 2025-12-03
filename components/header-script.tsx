5fa2c9a15dde6d094567b16df187768ff89715a5
thiagoedigital-del <thiago.edigital@gmail.com>
2025-12-03 20:32:52 -0300

    testando pixel meta

--- components/header-script.tsx
+++ components/header-script.tsx
@@ -1,9 +1,9 @@
 import Script from "next/script";
 
 const idList: Record<string, string> = {
-  kim: "68d2918cf1f6ffb0ac84056b",
-  elon: "68d2918cf1f6ffb0ac84056b",
-  shakira: "68d2918cf1f6ffb0ac84056b",
+  kim: "6930a849a6cbcd96bc532f47",
+  elon: "6930a849a6cbcd96bc532f47",
+  shakira: "6930a849a6cbcd96bc532f47",
 };
 
 export default function HeaderScript({ content }: { content: string }) {
@@ -14,6 +14,35 @@ export default function HeaderScript({ content }: { content: string }) {
 
   return (
     <>
+      {/* Facebook Pixel Base Code - Para detecção do Pixel Helper */}
+      <Script
+        id="facebook-pixel-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${pixelId}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+      {/* UTMify Scripts */}
       <Script
         id="utmify-config"
         strategy="afterInteractive"
